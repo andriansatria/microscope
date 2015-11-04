23453bd0b943fff7420a543a0029ac21022315dd
andriansatria <rian6487@gmail.com>
2015-11-04 21:09:24 +0700

    Commit 10-1
    Added comments collection, pub/sub and fixtures

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -1,11 +1,3 @@
-Router.configure({
-    layoutTemplate: 'layout',
-    loadingTemplate: 'loading',
-    waitOn: function() {
-        return Meteor.subscribe('posts');
-    }
-});
-
 Router.map(function() {
     
      this.route('postsList', {
