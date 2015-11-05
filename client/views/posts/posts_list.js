61372113ffb8041b952edee50bc160a69219eef6
andriansatria <rian6487@gmail.com>
2015-11-05 18:19:41 +0700

    Commit 12-4
    Refactored postsList route into a RouteController

--- client/views/posts/posts_list.js
+++ client/views/posts/posts_list.js
@@ -1,18 +1,6 @@
-/*var postsData = [{
-    title: 'Introducing Telescope',
-    author: 'Sacha Greif',
-    url: 'http://sachagreif.com/introducing-telescope/'
-}, {
-    title: 'Meteor',
-    author: 'Tom Coleman',
-    url: 'http://meteor.com'
-}, {
-    title: 'The Meteor Book',
-    author: 'Tom Coleman',
-    url: 'http://themeteorbook.com'
-}];*/
 Template.postsList.helpers({
-    posts: function() {
-        return Posts.find({}, {sort: {submitted: -1}});
+    hasMorePosts: function() {
+        this.posts.rewind();
+        return Router.current().postsLimit() == this.posts.fetch().length;
     }
 });
