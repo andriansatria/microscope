12c5d7472f6199735618592f1a758ccd185e4112
andriansatria <rian6487@gmail.com>
2015-11-06 17:54:34 +0700

    Commit 13-7
    add a little animation

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -8,10 +8,10 @@ Template.postItem.helpers({
         a.href = this.url;
         return a.hostname;
     },
-    
-    upvotedClass : function() {
+
+    upvotedClass: function() {
         var userId = Meteor.userId();
-        if(userId && !_.include(this.upvoters, userId)) {
+        if (userId && !_.include(this.upvoters, userId)) {
             return 'btn-primary upvotable';
         } else {
             return 'disabled';
@@ -19,6 +19,33 @@ Template.postItem.helpers({
     }
 
 });
+
+Template.postItem.rendered = function() {
+    // animate post from previous position to new position
+    var instance = this;
+    var rank = instance.data._rank;
+    var $this = $(this.firstNode);
+    var postHeight = 80;
+    var newPosition = rank * postHeight;
+    console.log(rank);
+    // if element has a currentPosition (i.e. it's not the first ever render)
+    if (typeof(instance.currentPosition) !== 'undefined') {
+        var previousPosition = instance.currentPosition;
+        // calculate difference between old position and new position and send element there
+        var delta = previousPosition - newPosition;
+        console.log(delta);
+        $this.css("top :", delta + "px");
+    } else {
+        $this.addClass("invisible")
+    }
+    // let it draw in the old position, then..
+    Meteor.defer(function() {
+        instance.currentPosition = newPosition;
+         console.log(instance.currentPosition);
+        // bring element back to its new original position
+        $this.css("top :", "0px").removeClass("invisible");
+    });
+};
 
 Template.postItem.events({
     'click .upvote': function(e) {
